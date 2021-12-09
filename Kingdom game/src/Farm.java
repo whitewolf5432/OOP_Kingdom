a28b9ceebb4a7dab8d7129e5b557efe8abde6805
KrittinKlinkaew63070004 <63070004@it.kmitl.ac.th>
2021-12-09 21:14:12 +0700

    Add Class & Interface

---
+++ Kingdom game/src/Farm.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+/**
+ *
+ * @author Administrator
+ */
+public class Farm extends JPanel{
+    
+    private int level;
+    private int value;
+    public void paintComponent(Graphics g) {
+        switch (this.getLevel()) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
+}

5a7850a0ce3c5d2e270fe0c00ffd02fc28b21d38
White wolf <59527394+whitewolf5432@users.noreply.github.com>
2021-12-13 19:09:21 +0700

    Update Build.java

--- Kingdom game/src/Build.java
+++ Kingdom game/src/Build.java
@@ -1,18 +1,32 @@
-import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.*;
+import java.util.*;
 import javax.swing.*;
 public class Build implements Runnable, KeyListener{
     private boolean state;
     public boolean running;
     public JFrame frame;
     private JPanel top, bot;
-    private JLabel test;
+    private JLabel labelChar;
+    private String lowerCase, upperCase, number, symbol, storeWord;
+    private int xxx;
+    private char rChar;
+    private Font font;
+    private ArrayList<JLabel> strMiniGame;
     
     public Build() {
         frame = new JFrame("Build...");
         top = new JPanel();
         bot = new JPanel();
+        font = new Font("Serif", Font.ITALIC, 45);
+        lowerCase = "abcdefghijklmnopqrstuvwxyz";
+        upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        number = "0123456789";
+        symbol = "!@#$%^&*()-_=+{[}]\\|;:'\"`~,<.>/?";
+        storeWord = "";
+        strMiniGame = new ArrayList();
         frame.setLayout(null);
         top.setBounds(0, 0, 410, 50);
         top.setBackground(Color.BLUE);
@@ -52,23 +66,48 @@ public class Build implements Runnable, KeyListener{
     public void topPanel() {
     }
     public void botPanel() {
-//        bot.setLayout(new GridLayout(2, 5));
-        for(int i=0; i < 30; i++) {
-            test = new JLabel("; ");
-            test.setFont(new Font("Serif", Font.ITALIC, 45));
-            bot.add(test);
+        for(int i=0; i < 10; i++) {
+            storeWord += lowerCase.charAt((int)(Math.random() * (lowerCase.length()-1)));
         }
-        System.out.println(bot.getLayout());
+        for(int i=0; i < 10; i++) {
+            storeWord += upperCase.charAt((int)(Math.random() * (upperCase.length()-1)));
+        }
+        for(int i=0; i < 10; i++) {
+            storeWord += number.charAt((int)(Math.random() * (number.length()-1)));
+        }
+        for(int i=0; i < 10; i++) {
+            storeWord += symbol.charAt((int)(Math.random() * (symbol.length()-1)));
+        }
+        List<String> letters = Arrays.asList(storeWord.split(""));
+        Collections.shuffle(letters);
+        for(String letter: letters) {
+            labelChar = new JLabel(letter + " ");
+            labelChar.setFont(font);
+            bot.add(labelChar);
+            strMiniGame.add(labelChar);
+        }
+        strMiniGame.get(xxx).setForeground(Color.GRAY);
+    }
+    public void fail() {
+        
+    }
+    public void done() {
+        state = true;
     }
     public static void main(String[] args) {
         new Build();
     }
     public void keyTyped(KeyEvent ke) {}
     public void keyPressed(KeyEvent ke) {
-        if(Character.isDefined(ke.getKeyChar()) && ke.getKeyChar() != KeyEvent.VK_ESCAPE && ke.getKeyChar() != KeyEvent.VK_BACK_SPACE) {
+        if(Character.isDefined(ke.getKeyChar()) && ke.getKeyChar() != KeyEvent.VK_ESCAPE && ke.getKeyChar() != KeyEvent.VK_BACK_SPACE && ke.getKeyChar() != KeyEvent.VK_SPACE) {
             System.out.println(ke.getKeyChar());
-            test.setText(ke.getKeyChar()+"");
-            test.setForeground(Color.getHSBColor((float)Math.random(), (float)Math.random(), (float)Math.random()));
+            if(strMiniGame.get(xxx).getText().charAt(0) == ke.getKeyChar()) {
+                strMiniGame.get(xxx).setForeground(Color.GREEN);
+            } else {
+                strMiniGame.get(xxx).setForeground(Color.RED);
+                fail();
+            }
+            strMiniGame.get(++xxx).setForeground(Color.GRAY);
         }
     }
     public void keyReleased(KeyEvent ke) {}

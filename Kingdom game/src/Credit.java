628e1f0a5fb5f073f4d5c7e4d2f320df8b998ff7
it63070045 <73681882+it63070045@users.noreply.github.com>
2021-12-16 00:26:18 +0700

    update credit

--- Kingdom game/src/Credit.java
+++ Kingdom game/src/Credit.java
@@ -27,6 +27,16 @@ public class Credit extends javax.swing.JPanel {
         jLabel5 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        jLabel13 = new javax.swing.JLabel();
+        jLabel14 = new javax.swing.JLabel();
+        jLabel15 = new javax.swing.JLabel();
+        jLabel16 = new javax.swing.JLabel();
+        jLabel17 = new javax.swing.JLabel();
 
         jPanel6.setBackground(new java.awt.Color(153, 42, 24));
         jPanel6.setMaximumSize(new java.awt.Dimension(1280, 800));
@@ -102,8 +112,67 @@ public class Credit extends javax.swing.JPanel {
 
         jPanel6.add(jPanel11);
         jPanel11.setBounds(150, 290, 120, 120);
+        
+        jLabel8.setFont(new java.awt.Font("Century", 0, 15)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel8.setText("Pimmada Krunsungnoen");
+        add(jLabel8);
+        jLabel8.setBounds(1000, 420, 200, 50);
 
+        jLabel9.setFont(new java.awt.Font("Century", 0, 15)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel9.setText("Krittin   Klinkaew");
+        add(jLabel9);
+        jLabel9.setBounds(130, 420, 200, 50);
 
+        jLabel10.setFont(new java.awt.Font("Century", 0, 15)); // NOI18N
+        jLabel10.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel10.setText("Chanapol   Chuenchuwit");
+        add(jLabel10);
+        jLabel10.setBounds(350, 420, 200, 50);
+
+        jLabel11.setFont(new java.awt.Font("Century", 0, 15)); // NOI18N
+        jLabel11.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel11.setText("Natanon   Hanchana");
+        add(jLabel11);
+        jLabel11.setBounds(570, 420, 200, 50);
+
+        jLabel12.setFont(new java.awt.Font("Century", 0, 15)); // NOI18N
+        jLabel12.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel12.setText("Nattapat   Buakamkote");
+        add(jLabel12);
+        jLabel12.setBounds(780, 420, 200, 50);
+
+        
+        jLabel13.setFont(new java.awt.Font("Century", 0, 15)); // NOI18N
+        jLabel13.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel13.setText("kuy kuy kuy");
+        add(jLabel13);
+        jLabel13.setBounds(1000, 450, 200, 50);
+
+        jLabel14.setFont(new java.awt.Font("Century", 0, 15)); // NOI18N
+        jLabel14.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel14.setText("kuy kuy kuy");
+        add(jLabel14);
+        jLabel14.setBounds(130, 450, 200, 50);
+
+        jLabel15.setFont(new java.awt.Font("Century", 0, 15)); // NOI18N
+        jLabel15.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel15.setText("kuy kuy kuy");
+        add(jLabel15);
+        jLabel15.setBounds(350, 450, 200, 50);
+
+        jLabel16.setFont(new java.awt.Font("Century", 0, 15)); // NOI18N
+        jLabel16.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel16.setText("kuy kuy kuy");
+        add(jLabel16);
+        jLabel16.setBounds(570, 450, 200, 50);
+
+        jLabel17.setFont(new java.awt.Font("Century", 0, 15)); // NOI18N
+        jLabel17.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel17.setText("kuy kuy kuy");
+        add(jLabel17);
+        jLabel17.setBounds(780, 450, 200, 50);
 
         setMaximumSize(new java.awt.Dimension(1280, 800));
         setMinimumSize(new java.awt.Dimension(1280, 800));
@@ -185,6 +254,16 @@ public class Credit extends javax.swing.JPanel {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel15;
+    private javax.swing.JLabel jLabel16;
+    private javax.swing.JLabel jLabel17;
     private javax.swing.JPanel jPanel10;
     private javax.swing.JPanel jPanel11;
     private javax.swing.JPanel jPanel6;

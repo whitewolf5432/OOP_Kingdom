6434203d9659d5fec9a27551a36fff533e6f7be4
White wolf <59527394+whitewolf5432@users.noreply.github.com>
2021-12-15 17:07:22 +0700

    remove extend JFrame

--- Kingdom game/src/Game.java
+++ Kingdom game/src/Game.java
@@ -8,7 +8,7 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.sound.sampled.*;
 
-public class Game extends JFrame{
+public class Game {
     private static JFrame frame;
     private static Play play;
     private static Home home;
@@ -18,38 +18,34 @@ public class Game extends JFrame{
     private static Ending end;
 
     public Game() {
-       frame = new JFrame("O(op) Kingdom");
-       play = new Play();
-       home = new Home();
-       credit = new Credit();
-       menu = new Menu();
-       tip = new Tips();
-       end = new Ending();
-       frame.setLayout(new BorderLayout());
-       frame.add(home);
-       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       frame.setResizable(false);
-       frame.setSize(1280, 740);
-       Thread music = new Thread() {
-	public void run() {
-		Clip clip;
-		try {
-			AudioInputStream input = AudioSystem.getAudioInputStream(new File("music/Guitar-Gentle.wav"));
-			clip = AudioSystem.getClip();
-			clip.open(input);
-                        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-                        gainControl.setValue(-25.0f);
-			clip.loop(Clip.LOOP_CONTINUOUSLY);
-			clip.start();
-		} catch (UnsupportedAudioFileException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
-		}
-	}
-};
+        frame = new JFrame("O(op) Kingdom");
+        play = new Play();
+        home = new Home();
+        credit = new Credit();
+        menu = new Menu();
+        tip = new Tips();
+        end = new Ending();
+        frame.setLayout(new BorderLayout());
+        frame.add(home);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(1280, 740);
+        Thread music = new Thread() {
+            public void run() {
+                Clip clip;
+                try {
+                    AudioInputStream input = AudioSystem.getAudioInputStream(new File("music/Guitar-Gentle.wav"));
+                    clip = AudioSystem.getClip();
+                    clip.open(input);
+                    FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+                    gainControl.setValue(-25.0f);
+                    clip.loop(Clip.LOOP_CONTINUOUSLY);
+                    clip.start();
+                } catch (IOException | LineUnavailableException | UnsupportedAudioFileException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
        music.start();
        frame.setLocationRelativeTo(null);       
        frame.setVisible(true);

85fd6ce4752113db9d423e45905f8450096f2707
KrittinKlinkaew63070004 <63070004@it.kmitl.ac.th>
2021-12-15 13:08:10 +0700

    Add bgm

--- Kingdom game/src/Game.java
+++ Kingdom game/src/Game.java
@@ -1,5 +1,12 @@
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
+import java.io.*;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.*;
 
 public class Game extends JFrame{
     private static JFrame frame;
@@ -23,8 +30,30 @@ public class Game extends JFrame{
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        frame.setResizable(false);
        frame.setSize(1280, 740);
+       Thread music = new Thread() {
+	public void run() {
+		Clip clip;
+		try {
+			AudioInputStream input = AudioSystem.getAudioInputStream(new File("music/Guitar-Gentle.wav"));
+			clip = AudioSystem.getClip();
+			clip.open(input);
+                        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+                        gainControl.setValue(-10.0f);
+			clip.loop(Clip.LOOP_CONTINUOUSLY);
+			clip.start();
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
+	}
+};
+       music.start();
        frame.setLocationRelativeTo(null);       
        frame.setVisible(true);
+       
     }
     public static void HowToPlay(int n){
         tip.setN(n);
